d2c344470206594ff6be3ae3216c72a7bdf4c0b8
Danzo-2989 <danisprima08@gmail.com>
2026-04-12 20:43:50 +0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -70,7 +70,7 @@ function Home() {
       </header>
 
       {/* Grid Produk */}
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
         {Object.entries(products).map(([id, product], index) => (
           <div 
             key={id} 
